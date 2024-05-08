52b3c84c7183ab314a4a2b54a1dcf0bcf4d24b10
Quls <Yudriqul@0803@gmail.com>
2024-05-08 14:55:09 +0700

    route again

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,17 @@ import Ticket from "./pages/Ticket";
 import Event from "./pages/Event";
 
 import NotFound from "./pages/NotFound";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         {/* Rute-rute untuk halaman Home, Event, dan Ticket */}
-        <Route index path="/tiketshop.io" element={<Home />} />
-        <Route path="/tiketshop.io/event" element={<Event />} />
-        <Route path="/tiketshop.io/ticket" element={<Ticket />} />
+        <Route index path="/" element={<Home />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/ticket" element={<Ticket />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
